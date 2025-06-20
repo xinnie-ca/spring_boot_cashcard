d2655693c12f7faf9fc046964fd8db3f30497a83
XinNie <umnie2@myumanitoba.ca>
2025-06-20 10:31:56 -0500

    m

--- src/main/java/com/example/cashcard/repository/CashCardRepository.java
+++ src/main/java/com/example/cashcard/repository/CashCardRepository.java
@@ -2,12 +2,13 @@ package com.example.cashcard.repository;
 import com.example.cashcard.model.CashCard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface CashCardRepository extends CrudRepository <CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+public interface CashCardRepository extends JpaRepository<CashCard, Long> {
     Optional<CashCard> findByIdAndOwner(Long Id, String owner);
     Page<CashCard> findByOwner(String owner, Pageable pageable);
     boolean existsByIdAndOwner(Long id, String owner);
