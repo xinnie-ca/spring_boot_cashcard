558262168e407728d327481ec5a4f6cb58f4602c
XinNie <umnie2@myumanitoba.ca>
2025-06-24 08:36:56 -0500

    remove unused import

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.cashcard;
 
 import com.example.cashcard.dto.CashCardBulkUpdateDTO;
 import com.example.cashcard.dto.CashCardRequestDTO;
-import com.example.cashcard.model.CashCard;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
